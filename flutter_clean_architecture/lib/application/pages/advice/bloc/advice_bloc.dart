9870c15a4d50a16f8c464d355b46f6272e7952e1
Oleksandr Volkov <formobileprima@gmail.com>
2023-05-18 15:26:15 +0300

    add base of bloc

---
+++ flutter_clean_architecture/lib/application/pages/advice/bloc/advice_bloc.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'advice_event.dart';
+part 'advice_state.dart';
+
+class AdviceBloc extends Bloc<AdviceEvent, AdviceState> {
+  AdviceBloc() : super(AdviceInitial()) {
+    on<AdviceRequestEvent>((event, emit) async {
+      emit(AdviceStateLoading());
+
+      debugPrint("Fake get ans advice");
+      await Future.delayed(const Duration(seconds: 3));
+      debugPrint("Fake advice loaded");
+
+      emit(AdviceStateLoaded(advice: "fake advice from bloc"));
+      // emit(AdviceStateError(advice: "Error message from bloc"));
+    });
+  }
+}
---
+++ flutter_clean_architecture/lib/application/pages/advice/bloc/advice_state.dart
@@ -0,0 +1,20 @@
+part of 'advice_bloc.dart';
+
+@immutable
+abstract class AdviceState {}
+
+class AdviceInitial extends AdviceState {}
+
+class AdviceStateLoading extends AdviceState {}
+
+class AdviceStateLoaded extends AdviceState {
+  final String advice;
+
+  AdviceStateLoaded({required this.advice});
+}
+
+class AdviceStateError extends AdviceState {
+  final String advice;
+
+  AdviceStateError({required this.advice});
+}
